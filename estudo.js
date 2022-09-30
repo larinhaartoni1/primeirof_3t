4b1ce9e919517a980d33d44ad1cf55856b43d536
larinhaartoni1 <108936713+larinhaartoni1@users.noreply.github.com>
2022-09-30 10:00:12 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,21 @@
+let tabuada = 2;
+
+
+function escreva(){
+    document.write("tabuada do " + tabuada + "<br>")
+    document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
+    document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
+    document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
+    document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
+    document.write(tabuada + " x 5 = " + (tabuada*5) + "<br>");
+    document.write(tabuada + " x 1 = " + (tabuada*6) + "<br>");
+    document.write(tabuada + " x 1 = " + (tabuada*7) + "<br>");
+    document.write(tabuada + " x 1 = " + (tabuada*8) + "<br>"); 
+    document.write(tabuada + " x 1 = " + (tabuada*9) + "<br>");
+    document.write(tabuada + " x 1 = " + (tabuada*10) + "<br>");
+}
+}
+}
+
+
 
