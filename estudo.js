7c03107c5ad6ebd44be46efff11be88443eeddfe
larinhaartoni1 <108936713+larinhaartoni1@users.noreply.github.com>
2022-10-20 11:12:42 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,4 +1,4 @@
-let tabuada = 5;
+let tabuada = 8;
 
 function escreva(){
     document.write("Tabuada do " + tabuada + "<br>")
@@ -13,31 +13,50 @@ function escreva(){
     document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
     document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
 }
-let lista = ["Jorge","Duarte","Waldete","Raquel"];
+
+let lista = ["Jorge","Duarte","Waldete","Raquel",];
 
 function mostra(){
-    document.write(lista.lenght + "<br>");
-    for(let i = 0; i < lista.lenght; i++){ 
-    document.write(lista[i] + "<br>");
+    document.write(lista.length + "<br>");
+    for(let i = 0; i < lista.length; i++){
+        document.write(lista[i] + "<br>");
     }
-    
 }
 
 function mostraTabuada(){
     for(let i = 1; i <= 10; i++){
         document.write("O valor do i " + i + "<br>");
     }
-
+}
 function total(){
     let v = document.getElementById("valor").value;
     let j = document.getElementById("juros").value;
     let t = document.getElementById("meses").value;
     let r = 0;
-
     for(let i =1; i <=t; i++){
         r = v * (1 + (j/100));
-        document.write("mes " + i + " - valor: " + r + "<br>");       
+        document.write("Mês " + i + " - valor: " + r + "<br>");
         v = r;
-
     }
-        document.write("resultado: " + r); 
+    document.write("Resultado: " + r);
+}
+
+function larasomanota(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
+    let r = Number(n1)+Number(n2)+Number(n3)+Number(n4);
+
+    document.getElementById("result").innerHTML = "soma: " + r;
+}
+
+function laraperdenota(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
+    let r = Number(n1)+Number(n2)+Number(n3)+Number(n4);
+
+    document.getElementById("result").innerHTML = "soma: " + r;
+}
